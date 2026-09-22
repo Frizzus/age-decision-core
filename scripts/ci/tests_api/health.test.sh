fca899152eb3a8f5214f90260a3bdfe05e3f34f8
frizzus <loic.canin@murena.io>
2026-09-22 22:21:09 +0200

    premier test pour le test /health dans usage.md

---
+++ scripts/ci/tests_api/health.test.sh
@@ -0,0 +1,27 @@
+set -eu
+
+# EXPECTED: Fichier contenant le résultat attendu
+# ACTUAL: Fichier contenant le résultat du test
+# PROJECT_FILE: Fichier project.json
+# COMPATIBILITY_FILE: Fichier compatibility.json
+
+get_value_from_project () {
+	jq -r ".$1" $PROJECT_FILE
+}
+
+curl --silent http://localhost:8000/health > $ACTUAL
+
+# jq -n couplé aux arguments --args permet d'échapper les caractères spéciaux.
+# le fichier project.json, ne devrait pas contenir ce genre de caractères, si il en contient le test devrait l'arrêter
+jq <<JSON > $EXPECTED
+{
+	status:"ok",
+	service:\"$(get_value_from_project service_name)\",
+	version: \"$(get_value_from_project version)\",
+	contract_version: \"$(get_value_from_project contract_version)\" 
+}
+JSON
+
+diff -u $ACTUAL $EXPECTED
+
+
