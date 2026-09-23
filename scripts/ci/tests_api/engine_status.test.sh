0faf1e984a6922ebcd872e57f2a792b21001db82
frizzus <loic.canin@murena.io>
2026-09-23 17:50:19 +0200

    python boolean not compatible with json boolean

--- scripts/ci/tests_api/engine_status.test.sh
+++ scripts/ci/tests_api/engine_status.test.sh
@@ -9,6 +9,12 @@ get_value_from_settings () {
 	awk "BEGIN {FS=\"=\"}; /$value/ {print \$2}" $INFRA_SETTINGS
 }
 
+# Python boolean always begin with capitalize T or F (True, False)
+# It is not a valid bool in json
+to_valid_bool () {
+	tr '[A-Z]' '[a-z]'
+}
+
 curl --silent http://localhost:8000/version | jq > $ACTUAL
 
 jq <<JSON > $EXPECTED
@@ -24,7 +30,7 @@ jq <<JSON > $EXPECTED
     "runtime": "onnx",
     "scoring_policy_id": $(get_value_from_settings age_scoring_policy_id),
     "mode": "onnx",
-    "use_mock_model": $(get_value_from_settings use_mock_model),
+    "use_mock_model": $(get_value_from_settings use_mock_model | to_valid_bool),
     "model_loaded": true,
     "output_supported": true
   }
